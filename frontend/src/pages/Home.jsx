52bbe0e9b3586cc07086d37f52615b90f7ccb4e2
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-02-13 22:48:49 -0500

    Configuracion del frontend lista

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Fondo from '../images/fondo_1.jpg';
+
+export default function Home(){
+    return(
+        <section style={{backgroundImage: `url(${Fondo})`}}>
+            Home
+            <br/>
+            g
+            <br/>
+            <br/>
+            <br/>
+            rf
+            <br/>
+            <br/>
+            <b>
+                rb
+            </b>
+            rrr
+            <br/>
+            <br/>
+            rrr
+            Home
+            <br/>
+            g
+            <br/>
+            <br/>
+            <br/>
+            rf
+            <br/>
+            <br/>
+            <b>
+                rb
+            </b>
+            rrr
+            <br/>
+            <br/>
+            rrr
+            Home
+            <br/>
+            g
+            <br/>
+            <br/>
+            <br/>
+            rf
+            <br/>
+            <br/>
+            <b>
+                rb
+            </b>
+            rrr
+            <br/>
+            <br/>
+            rrr
+            Home
+            <br/>
+            g
+            <br/>
+            <br/>
+            <br/>
+            rf
+            <br/>
+            <br/>
+            <b>
+                rb
+            </b>
+            rrr
+            <br/>
+            <br/>
+            rrr
+            Home
+            <br/>
+        
+
+        </section>
+    )
+}
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Fondo from '../images/fondo_1.jpg';
+
+export default function Login(){
+    return(
+        <section style={{backgroundImage: `url(${Fondo})`}}>
+            Login
+        </section>
+    )
+}
