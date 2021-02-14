a613d3a986e3a5c59f50c842e0edaece2290f365
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-02-14 03:28:14 -0500

    Inicio de sesión completo

---
+++ frontend/src/components/FormLogin.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import '../styles/styles.css';
+import {Link} from 'react-router-dom';
+import {Card} from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+import axios from '../axios/axios';
+import swal from 'sweetalert2';
+import {saveToLocal} from '../functions/LocalStorage';
+
+const Header=()=> {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data =>{
+        axios.post('/login', data).then(
+            (res)=>{
+                if(res.data[0]==undefined){
+                    swal.fire({
+                        title: "Información no registrada",
+                        text: "Ingrese bien su información o registrese primero",
+                        icon: "error",
+                        confirmButtonText: "¡Entendido!",
+                        confirmButtonColor: "#f96332",
+                    });
+                }else{
+                    const id = res.data[0]["_id"];
+                    saveToLocal("id", id);
+                    //window.location.href
+                } 
+            }
+        )
+    }
+        
+
+    return (
+        <div>
+            <div className="container d-flex my-5">
+                <h4 className='letter_tittle mt-2 text-white mx-auto'>Login</h4>
+            </div>
+            <Card className='mx-auto my-5 p-5 text-white bg-secondary' style={{ width: '25rem' }}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-3 pb-4">
+                        <label htmlFor="exampleInputEmail1" className="letter form-label">Email</label>
+                        <input required ref={register} type="email" className="text-white bg-secondary letter form-control" name="correo"/>
+                    </div>
+                    <div className="mb-1">
+                        <label htmlFor="exampleInputPassword1" className="letter form-label">Password</label>
+                        <input required ref={register} type="password" className="text-white bg-secondary letter form-control" name="contrasena"/>
+                    </div>
+                    <div className="letter pb-4">
+                        <Link className="text-white" style={{fontSize:15}} to="/singup">Registrarse</Link>
+                    </div>
+                    <div className="text-center text-white letter pb-2">
+                        <button type="submit" className="btn btn-secondary">Sign in</button>    
+                    </div>
+                </form> 
+            </Card>
+        </div>
+        );
+};
+
+export default Header;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/styles.css';
+import Logo from '../images/logo.png';
+import {Link} from 'react-router-dom';
+
+const Header=()=> {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-secondary">
+                <div>
+                    <Link to="/">
+                        <img style={{margin:5}} width="50px" src={Logo} alt="Logo" />
+                    </Link>
+                    <span style={{margin:5}} className="text-light letter">Progressive Tasks</span>
+                </div>
+            </nav>
+        </div>
+        );
+};
+
+export default Header;
