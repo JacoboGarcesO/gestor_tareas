2d6abe1ecdb303a176d5d7e8c3fd6cc3133d8ce9
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-02-15 22:19:14 -0500

    Solucionando errores de Mongo Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,9 @@
 const mongoose= require('mongoose');
 mongoose.connect('mongodb+srv://jacobo:joter123456789@gestor-tareas.dmbff.mongodb.net/gestor_tareas?retryWrites=true&w=majority',{
-    useNewUrlParser:true
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 }).then(db=>console.log(`Database connected.`))
 .catch(error=>console.error(error));
 
