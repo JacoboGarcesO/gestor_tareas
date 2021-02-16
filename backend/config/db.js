92fa36517ccdd921c92d90708842c6d3a2e89bfb
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-02-15 22:02:09 -0500

    Cambio a Mongo Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/gestor_tareas',{
+mongoose.connect('mongodb+srv://jacobo:joter123456789@gestor-tareas.dmbff.mongodb.net/gestor_tareas?retryWrites=true&w=majority',{
     useNewUrlParser:true
 }).then(db=>console.log(`Database connected.`))
 .catch(error=>console.error(error));
